904a46fbaad0b633531c59d2b3338700a8cd8705
Wallisson Marinho <wallissonmarinho@hotmail.com>
2020-05-07 02:17:59 -0300

    Header Adicionado

--- src/views/AbrirChamado.js
+++ src/views/AbrirChamado.js
@@ -1,16 +1,47 @@
 import * as React from 'react';
-import {View, Text, Button} from 'react-native';
+import {
+  Container,
+  Header,
+  Left,
+  Body,
+  Title,
+  View,
+  Thumbnail,
+  Text,
+} from 'native-base';
+import {Button, StyleSheet} from 'react-native';
 
 function AbrirChamado({navigation}) {
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Abrir Chamado</Text>
-      <Button
-        title="Ocorrencias"
-        onPress={() => navigation.navigate('Ocorrencias')}
-      />
-    </View>
+    <Container>
+      <Header androidStatusBarColor="#573ea8" style={styles.header} hasTabs>
+        {/* <Left>
+          <Thumbnail small source={{uri: logo_url}} />
+        </Left> */}
+        <Body>
+          <Title>Abrir Chamado</Title>
+        </Body>
+      </Header>
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text>Abrir Chamado</Text>
+        <Button
+          title="Ocorrencias"
+          onPress={() => navigation.navigate('Ocorrencias')}
+        />
+      </View>
+    </Container>
   );
 }
+
+const logo_url = 'https://avatars0.githubusercontent.com/u/28929274?s=200&v=4';
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: '#7159C1',
+  },
+  container: {
+    flex: 1,
+  },
+});
 
 export default AbrirChamado;
--- src/views/ChamadosAbertos.js
+++ src/views/ChamadosAbertos.js
@@ -1,13 +1,44 @@
 import * as React from 'react';
-import {View, Text, Button} from 'react-native';
+import {
+  Container,
+  Header,
+  Left,
+  Body,
+  Title,
+  View,
+  Thumbnail,
+  Text,
+} from 'native-base';
+import {Button, StyleSheet} from 'react-native';
 
 function ChamadosAbertos({navigation}) {
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Chamados Abertos</Text>
-      <Button title="Perfil" onPress={() => navigation.navigate('Perfil')} />
-    </View>
+    <Container>
+      <Header androidStatusBarColor="#573ea8" style={styles.header} hasTabs>
+        {/* <Left>
+          <Thumbnail small source={{uri: logo_url}} />
+        </Left> */}
+        <Body>
+          <Title>Abrir Chamado</Title>
+        </Body>
+      </Header>
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text>Chamados Abertos</Text>
+        <Button title="Perfil" onPress={() => navigation.navigate('Perfil')} />
+      </View>
+    </Container>
   );
 }
+
+const logo_url = 'https://avatars0.githubusercontent.com/u/28929274?s=200&v=4';
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: '#7159C1',
+  },
+  container: {
+    flex: 1,
+  },
+});
 
 export default ChamadosAbertos;
