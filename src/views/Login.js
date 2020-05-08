8c35f3cb1e411778868df43eec36e1791d80ef2f
Wallisson Marinho <wallissonmarinho@hotmail.com>
2020-05-08 01:14:34 -0300

    login construido

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,34 +1,41 @@
-import React from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import {Header, Container, Body, Title} from 'native-base';
+import React, { Component } from 'react';
+import { View, StyleSheet, KeyboardAvoidingView } from 'react-native';
+import { Form, Item, Input, Body, Text, CheckBox, Button, Header } from 'native-base';
 
-class Login extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { count: 0 };
-  }
-
+class Login extends Component {
   render() {
-    const { count } = this.state;
     return (
-      <Container>
-        <View style={{flex: 1}}>
-          <Header androidStatusBarColor="#0d83e0" style={styles.header} hasTabs>
-          <Body>
-            <Title>Login</Title>
-          </Body>
-        </Header>
-          <View style={styles.countContainer}>
-            <Text>Count: {count}</Text>
+      <KeyboardAvoidingView
+        behavior={Platform.OS == "ios" ? "padding" : "height"}
+        style={{ flex: 1 }}
+      >
+        <Header androidStatusBarColor="#0d83e0" style={styles.header} hasTabs />
+        <View style={styles.container}>
+          <View style={styles.top} />
+
+          <View style={styles.middle}>
+            <Text style={styles.textContainer}>ProInfra</Text>
+
+            <View style={styles.formArea}>
+              <Form style={styles.mainForm}>
+                <Item style={styles.formItems}>
+                  <Input placeholder="Seu email" style={styles.Input} />
+                </Item>
+                <Item style={styles.formItems}>
+                  <Input placeholder="Senha" style={styles.Input} />
+                </Item>
+
+                <View style={styles.Button}>
+                  <Button block style={styles.mainBtn} onPress={() => this.props.navigation.navigate('HomeTabs')}>
+                    <Text style={styles.btnText}>Entrar</Text>
+                  </Button>
+                </View>
+              </Form>
+            </View>
           </View>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => this.props.navigation.navigate('HomeTabs')}
-          >
-            <Text>Press Here</Text>
-          </TouchableOpacity>
+          <View style={styles.bottom}></View>
         </View>
-      </Container>
+      </KeyboardAvoidingView>
     );
   }
 }
@@ -39,16 +46,66 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
+    position: 'relative',
   },
-  button: {
-    alignItems: "center",
-    backgroundColor: "#DDDDDD",
-    padding: 10
+  top: {
+    position: 'relative',
+    backgroundColor: '#2196f3',
+    paddingRight: 12.7,
+    paddingLeft: 12.7,
+    height: 250,
   },
-  countContainer: {
-    alignItems: "center",
-    padding: 10
-  }
+  middle: {
+    width: '100%',
+    height: '100%',
+    flex: 1,
+    position: 'absolute',
+    zIndex: 2,
+    backgroundColor: 'transparent',
+    paddingLeft: 26.3,
+    paddingRight: 26.3,
+  },
+  bottom: {
+    position: 'relative',
+    height: '100%',
+    paddingRight: 12.7,
+    paddingLeft: 12.7,
+  },
+  textContainer: {
+    color: '#fff',
+    fontSize: 24,
+    marginBottom: 30,
+    position: 'relative',
+    top: '20%',
+    alignSelf: 'center',
+  },
+  formArea: {
+    alignSelf: 'center',
+    width: '100%',
+    backgroundColor: '#ffffff',
+    borderRadius: 5,
+    top: '20%',
+    paddingBottom: 40,
+  },
+  formItems: {
+    marginTop: 15,
+    borderBottomColor: '#2D3057',
+  },
+  Input: {
+    fontSize: 12,
+  },
+  Button: {
+    padding: 30.8,
+    borderRadius: 4,
+  },
+  mainBtn: {
+    backgroundColor: '#0d83e0',
+  },
+  btnText: {
+    color: '#fff',
+    fontFamily: 'GoogleSans-Medium',
+    fontSize: 12,
+  },
 });
 
 export default Login;
