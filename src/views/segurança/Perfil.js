f7643cc0840e91e96fa6618175608c14b9440278
Wallisson Marinho <wallissonmarinho@hotmail.com>
2020-06-07 19:58:00 -0300

    Perfil, add alterar dados e senha

--- src/views/segurança/Perfil.js
+++ src/views/segurança/Perfil.js
@@ -1,10 +1,13 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, ScrollView} from 'react-native';
 import {Header, Container, Body, Title, Button, Text} from 'native-base';
 import {Input} from '@ui-kitten/components';
 
 class Perfil extends React.Component {
+  state = {
+    perfil: 0,
+  };
   render() {
     return (
       <Container>
@@ -43,58 +46,175 @@ class Perfil extends React.Component {
                 />
               </View>
             </View>
-            <View style={{flex: 0.8}}>
-              <View style={{padding: 20, paddingTop: 80}}>
-                <Input
-                  disabled
-                  style={styles.Input}
-                  placeholder="Fulano XPTO"
-                  name="nomeCompleto"
-                />
-                <Input
-                  disabled
-                  style={styles.Input}
-                  placeholder="01/01/2020"
-                  name="data_nascimento"
-                  keyboardType="numeric"
-                />
+            <View style={{flex: 0.8, marginTop: 100}}>
+              {this.state.perfil === 0 && (
+                <ScrollView>
+                  <View style={{padding: 20}}>
+                    <Input
+                      disabled
+                      style={styles.Input}
+                      placeholder="Fulano XPTO"
+                      name="nomeCompleto"
+                    />
+                    <Input
+                      disabled
+                      style={styles.Input}
+                      placeholder="01/01/2020"
+                      name="data_nascimento"
+                      keyboardType="numeric"
+                    />
 
-                <Input
-                  disabled
-                  style={styles.Input}
-                  placeholder="(83) 9 2121-4343"
-                  name="telefone"
-                  keyboardType="numeric"
-                />
+                    <Input
+                      disabled
+                      style={styles.Input}
+                      placeholder="(83) 9 2121-4343"
+                      name="telefone"
+                      keyboardType="numeric"
+                    />
 
-                <Input
-                  disabled
-                  style={styles.Input}
-                  placeholder="emailxpto@xpto.com"
-                  name="email"
-                />
-                <View
-                  style={{
-                    justifyContent: 'space-between',
-                    flexDirection: 'row',
-                  }}>
-                  <Button
-                    block
-                    style={{backgroundColor: '#0d83e0', borderRadius: 4}}>
-                    <Text>Editar dados</Text>
-                  </Button>
-                  <Button
-                    block
-                    style={{backgroundColor: '#0d83e0', borderRadius: 4}}>
-                    <Text>Alterar Senha</Text>
-                  </Button>
-                </View>
-              </View>
-            </View>
-            <View>
-              <Button block style={{backgroundColor: 'red', marginBottom: 50}}>
-                <Text>Sair</Text>
-              </Button>
+                    <Input
+                      disabled
+                      style={styles.Input}
+                      placeholder="emailxpto@xpto.com"
+                      name="email"
+                    />
+                    <View
+                      style={{
+                        justifyContent: 'space-between',
+                        flexDirection: 'row',
+                      }}>
+                      <Button
+                        block
+                        onPress={() => this.setState({perfil: 1})}
+                        style={{backgroundColor: '#0d83e0', borderRadius: 4}}>
+                        <Text>Editar dados</Text>
+                      </Button>
+                      <Button
+                        block
+                        onPress={() => this.setState({perfil: 2})}
+                        style={{backgroundColor: '#0d83e0', borderRadius: 4}}>
+                        <Text>Alterar Senha</Text>
+                      </Button>
+                    </View>
+                  </View>
+                  <View>
+                    <Button
+                      block
+                      style={{
+                        backgroundColor: 'red',
+                        marginTop: 20,
+                      }}>
+                      <Text>Sair</Text>
+                    </Button>
+                  </View>
+                </ScrollView>
+              )}
+              {this.state.perfil === 1 && (
+                <ScrollView>
+                  <View style={{padding: 20}}>
+                    <Input
+                      style={styles.Input}
+                      placeholder="Fulano XPTO"
+                      name="nomeCompleto"
+                    />
+                    <Input
+                      style={styles.Input}
+                      placeholder="01/01/2020"
+                      name="data_nascimento"
+                      keyboardType="numeric"
+                    />
+
+                    <Input
+                      style={styles.Input}
+                      placeholder="(83) 9 2121-4343"
+                      name="telefone"
+                      keyboardType="numeric"
+                    />
+
+                    <Input
+                      style={styles.Input}
+                      placeholder="emailxpto@xpto.com"
+                      name="email"
+                    />
+                    <View
+                      style={{
+                        justifyContent: 'space-between',
+                        flexDirection: 'row',
+                      }}>
+                      <Button
+                        block
+                        onPress={() => this.setState({perfil: 0})}
+                        style={{backgroundColor: '#0d83e0', borderRadius: 4}}>
+                        <Text>Cancelar</Text>
+                      </Button>
+                      <Button
+                        block
+                        style={{backgroundColor: '#0d83e0', borderRadius: 4}}>
+                        <Text>Salvar</Text>
+                      </Button>
+                    </View>
+                  </View>
+                  <View>
+                    <Button
+                      block
+                      style={{
+                        backgroundColor: 'red',
+                        marginTop: 20,
+                      }}>
+                      <Text>Sair</Text>
+                    </Button>
+                  </View>
+                </ScrollView>
+              )}
+              {this.state.perfil === 2 && (
+                <ScrollView>
+                  <View style={{padding: 20}}>
+                    <Input
+                      style={styles.Input}
+                      placeholder="Digite sua senha"
+                      name="nomeCompleto"
+                    />
+                    <Input
+                      style={styles.Input}
+                      placeholder="Nova Senha"
+                      name="data_nascimento"
+                    />
+
+                    <Input
+                      style={styles.Input}
+                      placeholder="Confirmar nova senha"
+                      name="telefone"
+                    />
+                    <View
+                      style={{
+                        justifyContent: 'space-between',
+                        flexDirection: 'row',
+                      }}>
+                      <Button
+                        block
+                        onPress={() => this.setState({perfil: 0})}
+                        style={{backgroundColor: '#0d83e0', borderRadius: 4}}>
+                        <Text>Cancelar</Text>
+                      </Button>
+                      <Button
+                        block
+                        style={{backgroundColor: '#0d83e0', borderRadius: 4}}>
+                        <Text>Salvar</Text>
+                      </Button>
+                    </View>
+                  </View>
+                  <View>
+                    <Button
+                      block
+                      style={{
+                        backgroundColor: 'red',
+                        marginTop: 20,
+                      }}>
+                      <Text>Sair</Text>
+                    </Button>
+                  </View>
+                </ScrollView>
+              )}
             </View>
           </View>
         </View>
