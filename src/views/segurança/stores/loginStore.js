18e0e947da7be0761440ea955b3c1f23050f21d9
Matheus Lucas <matheuslucas.ssilva@gmail.com>
2020-06-21 03:18:21 -0300

    Correção no método logar

--- src/views/segurança/stores/loginStore.js
+++ src/views/segurança/stores/loginStore.js
@@ -49,26 +49,25 @@ class LoginStore {
     try {
       const result = await LoginService.login(this.login);
 
-      if (!result.headers.authorization || result.status === 404) {
-        mensagem.error('Erro ao efetuar o login! Por favor, tente novamente.');
-      }
-
-      this.usuario = await this.obterIdClienteLogado(
-        result.headers.authorization,
-      );
-
-      const subject = {
-        token: result.headers.authorization,
-        usuario: this.usuario,
-      };
-
-      await Subject.salvarSubject(subject);
-
       if (result.status === 401) {
         mensagem.error('Usuário e/ou senha incorreto(s)!');
       }
 
+      if (result.status === 403) {
+        mensagem.error('Usuário não encontrado!');
+      }
+
       if (result.status === 200) {
+        this.usuario = await this.obterIdClienteLogado(
+          result.headers.authorization,
+        );
+
+        const subject = {
+          token: result.headers.authorization,
+          usuario: this.usuario,
+        };
+
+        await Subject.salvarSubject(subject);
         navigation.navigate('HomeTabs');
         this.reset();
       }
